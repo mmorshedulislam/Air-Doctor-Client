f2b3c01bbc6cecebec6c85b0ac039d725c7c6f92
MORSHED-HOME <morshed952640@gmail.com>
2022-11-08 21:07:56 +0600

    'Google_sign_in_done'

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,10 +3,12 @@ import React, { useContext } from "react";
 import toast from "react-hot-toast";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Contexts/AuthProvider";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookSquare } from "react-icons/fa";
 
 const Login = () => {
-  const { signIn } = useContext(AuthContext);
-  
+  const { signIn, googleSignIn } = useContext(AuthContext);
+
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from?.pathname || "/";
@@ -17,13 +19,23 @@ const Login = () => {
     const email = form.email.value;
     const password = form.password.value;
 
-    signIn(email, password).then((result) => {
-      const user = result.user;
-      toast.success("Successfully Log In");
-      form.reset();
-      navigate(from, { replace: true });
-      console.log(user);
-    });
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        toast.success("Successfully Log In");
+        form.reset();
+        navigate(from, { replace: true });
+        console.log(user);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleGoogleLogin = () => {
+    googleSignIn()
+      .then(() => {
+        toast.success("Successfully login with Google");
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div>
@@ -64,6 +76,25 @@ const Login = () => {
           </div>
           <Button type="submit">Log In</Button>
         </form>
+        {/* HR line */}
+        <div class="inline-flex justify-center items-center w-full">
+          <hr class="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
+          <span class="absolute left-1/2 px-3 font-medium text-gray-900 bg-white -translate-x-1/2 dark:text-white dark:bg-gray-900">
+            or
+          </span>
+        </div>
+        <p className="text-xl text-center mb-3">Login with</p>
+        <div className="flex justify-center gap-5">
+          <button
+            onClick={handleGoogleLogin}
+            className="text-3xl rounded-full border p-2"
+          >
+            <FcGoogle />
+          </button>
+          <button className="text-3xl rounded-full border p-2 text-[#3b5998]">
+            <FaFacebookSquare />
+          </button>
+        </div>
       </div>
     </div>
   );
